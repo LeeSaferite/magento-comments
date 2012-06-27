e9268bd1b399bb22a7417e6846452fab8f195472
nathanmann <nathancodymann@gmail.com>
2012-06-27 16:05:23 -0500

    throw together untested getCollection

--- Model/Comment.php
+++ Model/Comment.php
@@ -6,6 +6,16 @@ class Nathan_Comments_Model_Comment extends Mage_Core_Model_Abstract
 	{
 		$this->_init('comments/comment');
 	}
+	
+	public function getCommentCollection($order_id) {
+		$comment = Mage::getModel('comments/comment')->getCollection()->addFilter('order_id', $order_id);
+		foreach($comment as $$comment_post) 
+		{
+			echo $comment_post->getComment()."<br />";
+			echo $comment_post->getTimestamp()."<br />";
+			echo $comment_post->getPosterid()."<br />";
+		}
+	}
 }
 
 ?>
