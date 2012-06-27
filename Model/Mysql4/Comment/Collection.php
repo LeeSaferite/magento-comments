dba0839fb70705b05190a969ef1989282cb65c8d
nathanmann <nathancodymann@gmail.com>
2012-06-27 16:17:27 -0500

    getCommentCollection() method and Collection.php

---
+++ Model/Mysql4/Comment/Collection.php
@@ -0,0 +1,11 @@
+<?php
+
+class Nathan_Comments_Model_Mysql4_Comment_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract 
+{
+	protected function _construct()
+	{
+		$this->_init('comments/comment');
+	}	
+}
+
+?>
--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -4,8 +4,7 @@ class Nathan_Comments_IndexController extends Mage_Core_Controller_Front_Action
 {
 	public function testAction() {
 		$coment = Mage::getModel('comments/comment');
-		$coment->load(1);
-		var_dump($coment->getData());
+		$coment->getCommentCollection(1);
 	}
 }
 
