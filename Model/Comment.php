0f5792e8d8c035dd997e2b92770b0973a8f95785
nathanmann <nathancodymann@gmail.com>
2012-06-28 13:42:26 -0500

    broken pos

--- Model/Comment.php
+++ Model/Comment.php
@@ -12,13 +12,29 @@ class Nathan_Comments_Model_Comment extends Mage_Core_Model_Abstract
 	 * @param
 	 * int     $order_id
 	 */
-	public function getCommentCollectionByOrderId($order_id) {
-		{
-			$collection = Mage::getModel('comments/comment')->getCollection();
-			$collection->addFilter('order_id', $order_id);
-			return $collection;
-		}
+	public function getCommentCollectionByOrderId($order_id) 
+	{	
+		$collection = Mage::getModel('comments/comment')->getCollection();
+		$collection->addFilter('order_id', $order_id);
+		return $collection;
 	}
+	
+	/**
+	 * add new comment
+	 * 
+	 * @param
+	 * int     $order_id
+	 * text    $comment
+	 * int     $poster_id
+	 */
+	 public function addComment($order_id, $poster_id, $comment) 
+	 {
+	 	$comment = Mage::getModel('comments/comment');
+		$comment->setOrderId(1);
+		$comment->setComment("comment");
+		$comment->setPosterId(2);
+		$comment->save();
+	 }
 }
 
 ?>
--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,8 +3,15 @@
 class Nathan_Comments_IndexController extends Mage_Core_Controller_Front_Action
 {
 	public function testAction() {
-		$coment = Mage::getModel('comments/comment');
-		$coment->getCommentCollection(1);
+		
+		$comment = Mage::getModel('comments/comment');
+		//$comment->addComment(1, 1, "asdf");
+		$comment->getCollection();
+		zend_debug::dump($comment->getData());
+		foreach($comment as $comment_print)
+		{
+			echo $comment_print->getComment()."<br />".$comment_print->getOrderId()."<br />";
+		}
 	}
 }
 
