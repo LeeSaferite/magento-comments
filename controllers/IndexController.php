d09fe31479bb46fe14177db0d41837f65ea440ad
nathanmann <nathancodymann@gmail.com>
2012-06-28 14:25:53 -0500

    update

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -4,14 +4,11 @@ class Nathan_Comments_IndexController extends Mage_Core_Controller_Front_Action
 {
 	public function testAction() {
 		
-		$comment = Mage::getModel('comments/comment');
-		//$comment->addComment(1, 1, "asdf");
-		$comment->getCollection();
-		zend_debug::dump($comment->getData());
-		foreach($comment as $comment_print)
+		foreach(Mage::getModel('comments/comment')->getByOrderId(1) as $comment_print)
 		{
 			echo $comment_print->getComment()."<br />".$comment_print->getOrderId()."<br />";
 		}
+		Mage::getModel('comments/comment')->addComment(1, 1, "a comment would like to worl");
 	}
 }
 
