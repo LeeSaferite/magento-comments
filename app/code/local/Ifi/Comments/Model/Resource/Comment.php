505a9bcc3761a9cb9321fcd1637a3b2e3b5148b9
Lee Saferite <lee.saferite@lokeycoding.com>
2012-06-29 11:52:31 -0400

    Fixing to work with Magento 1.3

--- app/code/local/Ifi/Comments/Model/Resource/Comment.php
+++ app/code/local/Ifi/Comments/Model/Resource/Comment.php
@@ -1,6 +1,6 @@
 <?php
 
-class Ifi_Comments_Model_Resource_Comment extends Mage_Core_Model_Resource_Db_Abstract
+class Ifi_Comments_Model_Resource_Comment extends Mage_Core_Model_Mysql4_Abstract
 {
     protected function _construct()
     {
--- app/code/local/Ifi/Comments/Model/Resource/Comment/Collection.php
+++ app/code/local/Ifi/Comments/Model/Resource/Comment/Collection.php
@@ -1,6 +1,6 @@
 <?php
 
-class Ifi_Comments_Model_Resource_Comment_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+class Ifi_Comments_Model_Resource_Comment_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
 {
     protected function _construct()
     {
